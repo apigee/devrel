494a7fc83274b06fdf4ee6e77dc59d81b3975d32
LaughingBiscuit <sean@laughingbiscuit.com>
2020-07-22 13:42:06 +0000

    relative path for expired token tests on aac + update apigetool command for apigee-sandbox-v1

--- tools/another-apigee-client/test/inject-expired-token.sh
+++ tools/another-apigee-client/test/inject-expired-token.sh
@@ -14,15 +14,16 @@
 # limitations under the License.
 
 
-set -x
 set -e
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
 if [ "$1" == "access" ]; then 
   OLD_TOKEN=$(echo "eyJhbGciOiJSUzI1NiJ9."`echo -n '{"exp":1593773053}' | base64`".o8EC22VBvh7Q5S1M3MQLAgMtfoo1OdXC")
   cat /root/.aac/token | jq --arg OLD_TOKEN $OLD_TOKEN '.access_token=$OLD_TOKEN' > /tmp/token
   mv /tmp/token $HOME/.aac/token
 elif [ "$1" == "refresh" ]; then
-  cp $HOME/aac/test/expired $HOME/.aac/token
+  cp $SCRIPTPATH/expired $HOME/.aac/token
 else
   exit 1
 fi
