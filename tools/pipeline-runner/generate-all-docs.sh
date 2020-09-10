b77ecaa4d89cafe4c2c45f1525fd6d321870812f
Daniel Strebel <strebel@google.com>
2020-09-10 13:01:39 +0200

    fix generated folder check

--- tools/pipeline-runner/generate-all-docs.sh
+++ tools/pipeline-runner/generate-all-docs.sh
@@ -75,9 +75,9 @@ for TYPE in references labs tools; do
   echo "<h2>$TYPE</h2>" >> ./generated/index.html
   echo "<ul>" >> ./generated/index.html
   for D in `ls $TYPE`; do
-    if [ -d  ./$TYPE/$D/generated/docs ]; then
-      (cd ./$TYPE/$D && ./generate-docs.sh;)
-      cp -r ./$TYPE/$D/generated/docs ./generated/$TYPE/$D  2>/dev/null || echo "NO DOCS FOR $TYPE/$D"
+    (cd ./$TYPE/$D && ./generate-docs.sh;)
+    cp -r ./$TYPE/$D/generated/docs ./generated/$TYPE/$D  2>/dev/null || echo "NO DOCS FOR $TYPE/$D"
+    if [ -d  ./generated/$TYPE/$D ]; then
       echo "<li><a href=\"./$TYPE/$D\">$D</a>" >> ./generated/index.html
     else 
       echo "<li><a href=\"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/tree/main/$TYPE/$D\">$D</a>" >> ./generated/index.html
