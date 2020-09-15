e1c0a7ee5ca6440b3413bd293904c186194689cc
laughingbiscuit <sean@laughingbiscuit.com>
2020-09-15 17:21:00 +0000

    set steps path

--- tools/pipeline-runner/run-steps.sh
+++ tools/pipeline-runner/run-steps.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-for CMD in $(cat steps.json | jq '.steps[]' -r); do
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+for CMD in $(cat $SCRIPTPATH/steps.json | jq '.steps[]' -r); do
   $CMD
 done
