da0d65364e72b6fc062fb2eefe985c7f53a8e5c3
Daniel Strebel <strebel@google.com>
2020-10-14 14:41:19 +0200

    Populate KVM as part of pipeline.sh

--- references/gcp-sa-auth-shared-flow/pipeline.sh
+++ references/gcp-sa-auth-shared-flow/pipeline.sh
@@ -13,4 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# REQUIREMENT:
+# Populate a service account key into the environment variable "REF_GCP_SA_SF" e.g. by: 
+# $ REF_GCP_SA_SF=$(cat /path/to/gcp-sa-key.json | jq '. | tostring')
+
+deleteKVM() {
+    curl -XDELETE -u "$APIGEE_USER:$APIGEE_PASS" "https://api.enterprise.apigee.com/v1/o/$APIGEE_ORG/e/$APIGEE_ENV/keyvaluemaps/$1"
+}
+
+#clean up if the KVM already exists
+deleteKVM 'gcp-sa-devrel'
+
+sleep 1
+
+#don't continue on failure
+set -e
+
+curl -XPOST -u "$APIGEE_USER:$APIGEE_PASS" "https://api.enterprise.apigee.com/v1/o/$APIGEE_ORG/e/$APIGEE_ENV/keyvaluemaps" \
+  -H 'Content-Type: application/json; charset=utf-8' \
+  --data-binary @- << EOF
+{
+  "name": "gcp-sa-devrel",
+  "encrypted": "true",
+  "entry": [
+    {
+      "name": "cantdonothing@iam.gserviceaccount.com",
+      "value": $REF_GCP_SA_SF
+    }
+  ]
+}
+EOF
+
 npm run test
+
+sleep 1
+
+# clean up
+deleteKVM 'gcp-sa-devrel'
