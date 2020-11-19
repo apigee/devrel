dd861808a6e6dfe6b610ad1f86c70fb0fe92aeb1
Joel Gauci <joelgauci@gmail.com>
2020-11-19 22:04:48 +0100

    initial version

--- references/dummy/pipeline.sh
+++ references/dummy/pipeline.sh
@@ -136,7 +136,7 @@ set_devapp_product() {
 set_idp_env_var
 
 # deploy Apigee artifacts: proxy, developer, app, product cache, kvm and proxy
-mvn install -P"$APIGEE_ENV" -Dapigee.config.options=update 
+mvn install -P"$APIGEE_ENV" -Dapigee.config.options=update -e
 
 # set developer app (apigee_client) credentials with the exact same values than the one in the keycloak IdP
 set_devapp_credentials
