8dffb534be5061b5617d1a2c689516d44544963d
laughingbiscuit <sean@laughingbiscuit.com>
2021-03-24 21:14:54 +0000

    shellcheck fixes

--- tools/apigee-openlegacy/pipeline.sh
+++ tools/apigee-openlegacy/pipeline.sh
@@ -5,15 +5,15 @@ set -e
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-mkdir -p $SCRIPTPATH/pipeline-workspace
-cd $SCRIPTPATH/pipeline-workspace
+mkdir -p "$SCRIPTPATH"/pipeline-workspace
+cd "$SCRIPTPATH"/pipeline-workspace
 
 # Install OpenLegacy cli
-if ! [ -f $SCRIPTPATH/pipeline-workspace/openlegacy-cli.zip ]; then
+if ! [ -f "$SCRIPTPATH"/pipeline-workspace/openlegacy-cli.zip ]; then
   curl -O https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.30.0/linux-macos/openlegacy-cli.zip
   unzip openlegacy-cli.zip
 fi
 
 # call the script with ol and gcloud on our path
-PATH=$PATH:$SCRIPTPATH/pipeline-workspace/ol/bin:/google-cloud-sdk/bin \
-  sh $SCRIPTPATH/bin/apigee-openlegacy.sh
+PATH="$PATH":"$SCRIPTPATH"/pipeline-workspace/ol/bin:/google-cloud-sdk/bin \
+  sh "$SCRIPTPATH"/bin/apigee-openlegacy.sh
