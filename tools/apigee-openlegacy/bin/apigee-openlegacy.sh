032d841c2f38d919ac428f38c30f2a6afb258711
laughingbiscuit <sean@laughingbiscuit.com>
2021-03-24 13:05:40 +0000

    various env changes

--- tools/apigee-openlegacy/bin/apigee-openlegacy.sh
+++ tools/apigee-openlegacy/bin/apigee-openlegacy.sh
@@ -34,7 +34,7 @@ fi
 # Check for required tools on path
 ###
 
-for TOOL in unzip ol gcloud jq nodejs npm; do
+for TOOL in unzip ol gcloud jq node npm; do
   if ! which $TOOL > /dev/null; then
     echo "Please ensure $TOOL is installed and on your PATH"
     exit 1
@@ -45,19 +45,19 @@ done
 # Configure OpenLegacy
 ###
 
-#ol login --api-key "$OPENLEGACY_APIKEY"
-#ol create module --connector as400-pcml aok-module
-#cp $SCRIPTPATH/../res/getcst.pcml aok-module/
-#(cd aok-module/ && ol add --source-path getcst.pcml --host "$OPENLEGACY_HOST" --code-page "$OPENLEGACY_CODEPAGE" --user "$OPENLEGACY_USER" --password "$OPENLEGACY_PASS")
-#(cd aok-module/ && ol push module)
-#ol create project aok-project --modules aok-module
+ol login --api-key "$OPENLEGACY_APIKEY"
+ol create module --connector as400-pcml aok-module
+cp $SCRIPTPATH/../res/getcst.pcml aok-module/
+(cd aok-module/ && ol add --source-path getcst.pcml --host "$OPENLEGACY_HOST" --code-page "$OPENLEGACY_CODEPAGE" --user "$OPENLEGACY_USER" --password "$OPENLEGACY_PASS")
+(cd aok-module/ && ol push module)
+ol create project aok-project --modules aok-module
 
 ###
 # Push OpenLegacy Image
 ###
 
-#gcloud services enable containerregistry.googleapis.com run.googleapis.com
-#gcloud auth configure-docker -q
+gcloud services enable containerregistry.googleapis.com run.googleapis.com
+gcloud auth configure-docker -q
 
 cat > Dockerfile <<EOF
 FROM openlegacy/as400-rpc:1.1.2
@@ -73,38 +73,38 @@ RUN echo '{ \
 }' > /tmp/data/config.json
 EOF
 
-#docker build -t gcr.io/$GCP_PROJECT/aok-image:latest .
-#docker push gcr.io/$GCP_PROJECT/aok-image:latest
+docker build -t gcr.io/$GCP_PROJECT/aok-image:latest .
+docker push gcr.io/$GCP_PROJECT/aok-image:latest
 
 
 ###
 # Deploy OpenLegacy
 ###
 
-#gcloud run deploy aok-service --image gcr.io/$GCP_PROJECT/aok-image:latest --platform managed --region europe-west1 -q
-FUNCTION_URL=$(gcloud run services describe aok-service --platform managed --region europe-west1 --format json | jq -r '.status.url')
+gcloud run deploy aok-service --image gcr.io/$GCP_PROJECT/aok-image:latest --platform managed --region europe-west1 -q
+TARGET_URL=$(gcloud run services describe aok-service --platform managed --region europe-west1 --format json | jq -r '.status.url')
 
 ###
 # Get OpenLegacy OpenAPI Specification
 ###
 
-#curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
-#  "$FUNCTION_URL/openapi/openapi.yaml" \
-#  -o openapi.yaml
+curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
+  "$TARGET_URL/openapi/openapi.yaml" \
+  -o openapi.yaml
 
 ###
 ## Generate Service Account for Apigee to call Cloud Run
 ###
 
-#gcloud iam service-accounts create aok-sa --project $GCP_PROJECT
-#gcloud iam service-accounts keys create credentials.json --iam-account aok-sa@$GCP_PROJECT.iam.gserviceaccount.com
-#gcloud run services add-iam-policy-binding aok-service --region europe-west1 --member serviceAccount:aok-sa@$GCP_PROJECT.iam.gserviceaccount.com --role roles/run.invoker --platform managed
+gcloud iam service-accounts create aok-sa --project $GCP_PROJECT || true
+gcloud iam service-accounts keys create credentials.json --iam-account aok-sa@$GCP_PROJECT.iam.gserviceaccount.com
+gcloud run services add-iam-policy-binding aok-service --region europe-west1 --member serviceAccount:aok-sa@$GCP_PROJECT.iam.gserviceaccount.com --role roles/run.invoker --platform managed || true
 
 ###
 # Create Apigee Cache for Service Account
 ###
 
-npx apigeetool createcache -u "$APIGEE_USER" -p "$APIGEE_PASS" -o "$APIGEE_ORG" -e "$APIGEE_ENV" -z aok-gcp-tokens
+npx apigeetool createcache -u "$APIGEE_USER" -p "$APIGEE_PASS" -o "$APIGEE_ORG" -e "$APIGEE_ENV" -z gcp-tokens || true
 
 ###
 # Push service account json to KVM
@@ -130,14 +130,16 @@ EOF
 # Deploy Shared Flow to manage JWT token to OpenLegacy
 ###
 
-npm run deploy --prefix $SCRIPTPATH/../../references/gcp-sa-auth-shared-flow
+npm run deploy --prefix $SCRIPTPATH/../../../references/gcp-sa-auth-shared-flow
 
 ###
 # Generate the Apigee Proxy
 ###
 
-cp -r $SCRIPTPATH/res/proxy aok-v1
-#SPEC=./target/openapi.json OPERATION=operationId oas-to-am > target/proxy/policies/Assign.OpenLegacy.xml
+cp -r $SCRIPTPATH/../res/proxy aok-v1
+sed -i.bak "s|@TargetURL@|$TARGETURL|" ./aok-v1/apiproxy/targets/default.xml
+sed -i.bak "s|@TargetURL@|$TARGETURL|" ./aok-v1/apiproxy/policies/AM.GCPAudience.xml
+rm ./aok-v1/apiproxy/targets/default.xml.bak ./aok-v1/apiproxy/policies/AM.GCPAudience.xml.bak
 
 ###
 # deploy apigee proxy
