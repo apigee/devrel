340d3a672c46f5aa76cf1a3d566a738a1d38b76f
Tyler Ayers <tyler.ayers@gmail.com>
2021-09-02 08:35:09 +0200

    Update deploy.sh
    changed env names to APIGEE_X_

--- references/data-converters-shared-flow/deploy.sh
+++ references/data-converters-shared-flow/deploy.sh
@@ -1,6 +1,6 @@
 TOKEN=$(gcloud auth print-access-token)
 zip -r sf-data-converters.zip sharedflowbundle
-DEPLOYRESULT=$(curl -X POST -H "Content-Type:multipart/form-data" -H "Authorization:Bearer $TOKEN" -F "file=@\"./sf-data-converters.zip\" type=application/zip;filename=\"sf-data-converters.zip\"" 'https://apigee.googleapis.com/v1/organizations/'$ORG'/sharedflows?name=SF-Data-Converters&action=import')
+DEPLOYRESULT=$(curl -X POST -H "Content-Type:multipart/form-data" -H "Authorization:Bearer $TOKEN" -F "file=@\"./sf-data-converters.zip\" type=application/zip;filename=\"sf-data-converters.zip\"" 'https://apigee.googleapis.com/v1/organizations/'$APIGEE_X_ORG'/sharedflows?name=SF-Data-Converters&action=import')
 echo "$DEPLOYRESULT"
 REVISION=$(jq '.revision' <<< "$DEPLOYRESULT")
 echo "$REVISION"
@@ -8,5 +8,5 @@ NEWREV="${REVISION%\"}"
 NEWREV="${NEWREV#\"}"
 echo "$NEWREV"
 #gcloud apigee apis deploy $NEWREV --environment=$ENV --api=SF-Data-Converters --override
-UPDATERESULT=$(curl -X POST -H "Authorization:Bearer $TOKEN" 'https://apigee.googleapis.com/v1/organizations/'$ORG'/environments/'$ENV'/sharedflows/SF-Data-Converters/revisions/'$NEWREV'/deployments' -d "override=true")
+UPDATERESULT=$(curl -X POST -H "Authorization:Bearer $TOKEN" 'https://apigee.googleapis.com/v1/organizations/'$APIGEE_X_ORG'/environments/'$APIGEE_X_ENV'/sharedflows/SF-Data-Converters/revisions/'$NEWREV'/deployments' -d "override=true")
 echo "$UPDATERESULT"
