774c80d07d815f246838b3b9856961c2613d9a58
Kurt Kanaskie <kurtkanaskie@google.com>
2022-01-05 18:03:10 -0500

    Switched to exec-maven-plugin since frontend-maven-plugin has issues with Alpine

--- references/product-recommendations/pipeline.sh
+++ references/product-recommendations/pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck disable=SC2181
 # Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
-echo Pipeline for product-recommendations-api in project: "$PROJECT_ID"
+echo Pipeline for product-recommendations in project: "$PROJECT_ID"
 
 # Environment variables
 echo APIGEE_X_ORG="$APIGEE_X_ORG" APIGEE_X_ENV="$APIGEE_X_ENV" APIGEE_X_HOSTNAME="$APIGEE_X_HOSTNAME"
@@ -30,13 +29,13 @@ export SPANNER_DATABASE=product-catalog-v1
 export SPANNER_REGION=regional-us-east1
 export SA=datareader@"$PROJECT_ID".iam.gserviceaccount.com
 
-echo "Pipeline for product-recommendations-api - setup bigquery"
+echo "Pipeline for product-recommendations - setup bigquery"
 ./setup_bigquery.sh
 
-echo "Pipeline for product-recommendations-api - setup spanner"
+echo "Pipeline for product-recommendations - setup spanner"
 ./setup_spanner.sh
 
-echo "Pipeline for product-recommendations-api - maven apigee install"
+echo "Pipeline for product-recommendations - maven apigee install"
 # This performs end-to-end install, configuration and testing API.
 mvn -P eval clean install -Dbearer="$(gcloud auth print-access-token)" \
     -DapigeeOrg="$APIGEE_X_ORG" \
@@ -45,17 +44,23 @@ mvn -P eval clean install -Dbearer="$(gcloud auth print-access-token)" \
     -DgcpProjectId="$PROJECT_ID" \
     -DgoogleTokenEmail="$SA" \
     -DintegrationTestUserId="$CUSTOMER_USERID"
+if [ $? != 0 ]; then
+    echo "Pipeline for product-recommendations - maven apigee install failed, cleaning up bigquery and spanner"
+    ./cleanup_bigquery.sh
+    ./cleanup_spanner.sh
+    exit 1
+fi
 
-echo "Pipeline for product-recommendations-api - maven apigee clean"
+echo "Pipeline for product-recommendations - maven apigee clean"
 mvn -P eval process-resources -Dbearer="$(gcloud auth print-access-token)" \
     -DapigeeOrg="$APIGEE_X_ORG" -DapigeeEnv="$APIGEE_X_ENV" -Dskip.integration=true \
     apigee-config:apps apigee-config:apiproducts apigee-config:developers -Dapigee.config.options=delete \
     apigee-enterprise:deploy -Dapigee.options=clean
 
-echo "Pipeline for product-recommendations-api - cleanup bigquery"
+echo "Pipeline for product-recommendations - cleanup bigquery"
 ./cleanup_bigquery.sh			
 
-echo "Pipeline for product-recommendations-api - cleanup spanner"
+echo "Pipeline for product-recommendations - cleanup spanner"
 ./cleanup_spanner.sh
 
-echo "Pipeline for product-recommendations-api - all done"
+echo "Pipeline for product-recommendations - all done"
