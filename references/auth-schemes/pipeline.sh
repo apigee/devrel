04116a38f7e98e29b838a51a6b57b2e52277c4fc
Omid Tahouri <omidt@google.com>
2022-02-14 21:19:30 +0000

    better use of jq to satisfy linter

--- references/auth-schemes/pipeline.sh
+++ references/auth-schemes/pipeline.sh
@@ -118,10 +118,10 @@ appId=$(sackmesser list --googleapi -t "$googleToken" "organizations/$APIGEE_X_O
 appJson=$(sackmesser list --googleapi -t "$googleToken" "organizations/$APIGEE_X_ORG/apps/$appId" | jq '.credentials[0]')
 
 # extract client id and secret from newly created app
-APP_CLIENT_ID=$(jq -r  '.consumerKey' <<< "$appJson")
+APP_CLIENT_ID=$(echo $appJson | jq -r  '.consumerKey')
 export APP_CLIENT_ID
 
-APP_CLIENT_SECRET=$(jq -r  '.consumerSecret' <<< "$appJson")
+APP_CLIENT_SECRET=$(echo $appJson | jq -r  '.consumerSecret')
 export APP_CLIENT_SECRET
 
 # var is expected by integration test (apickli)
