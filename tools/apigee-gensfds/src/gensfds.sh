3c61bcb56ebfe5d7eca725cdcb86f0fb49798432
Yuriy Lesyuk <yuriylesyuk@gmail.com>
2022-05-17 12:44:10 +0100

    Fix script lint errors

--- tools/apigee-gensfds/src/gensfds.sh
+++ tools/apigee-gensfds/src/gensfds.sh
@@ -35,16 +35,16 @@ fi
 # TODO: [ ] check for valid value
 
 
-cd "$sfdir"
+cd "$sfdir" || exit
 
-if [ $target = "dot" ]; then
+if [ "$target" = "dot" ]; then
 
   echo "digraph G {"
   echo "  rankdir=LR"
   echo "  node [shape=box,fixedsize=true,width=3]"
 fi
 
-if [ $target = "tsort" ]; then
+if [ "$target" = "tsort" ]; then
 
     dag=""
 
@@ -57,34 +57,34 @@ for sf in */; do
   sflow="${sf::-1}"
 
   # check for structural integrity
-  if [ ! -e ${sf}sharedflowbundle/policies ];then
+  if [ ! -e "${sf}sharedflowbundle/policies" ];then
      echo "ERROR: $sf directory is not a shared flow file structure. Missing: ${sf}sharedflowbundle/policies"
      exit 1
   fi
 
-  pushd ${sf}sharedflowbundle/policies > /dev/null
+  pushd "${sf}sharedflowbundle/policies" > /dev/null || exit
 
-if [ $target = "dot" ]; then
+if [ "$target" = "dot" ]; then
 
   list=$(grep -Ril "<FlowCallout " .)
   if [ -z "$list" ];then
-    printf "  \"%s\";\n" $sflow
+    printf "  \"%s\";\n" "$sflow"
   else
     for fc in $list; do
-      printf "  \"%s\" ->" $sflow
+      printf "  \"%s\" ->" "$sflow"
 
-      grep '<SharedFlowBundle' $fc | awk -F "[><]" '{print" \"" $3 "\";"}'
+      grep '<SharedFlowBundle' "$fc" | awk -F "[><]" '{print" \"" $3 "\";"}'
      done
   fi
 fi
 
-if [ $target = "tsort" ]; then
+if [ "$target" = "tsort" ]; then
   list=$(grep -Ril "<FlowCallout " .)
   if [ -z "$list" ];then
-          orphans+=($sflow)
+          orphans+=("$sflow")
   else
     for fc in $list; do
-      to="$(grep '<SharedFlowBundle' $fc | awk -F "[><]" '{print $3 "" }')"
+      to="$(grep '<SharedFlowBundle' "$fc" | awk -F "[><]" '{print $3 "" }')"
       visited[$to]=true
       dag="$dag$sflow $to"$'\n'
     done
@@ -92,16 +92,16 @@ if [ $target = "tsort" ]; then
 
 
 fi
-  popd > /dev/null
+  popd > /dev/null || exit
 
 done
 
-if [ $target = "dot" ]; then
+if [ "$target" = "dot" ]; then
   echo "}"
 fi
 
-if [ $target = "tsort" ]; then
-    echo "$(echo -n $dag|tsort|tac)"
+if [ "$target" = "tsort" ]; then
+    echo -n "$dag"|tsort|tac
 
     for sf in "${orphans[@]}"; do
       if [ ! "${visited[$sf]}" ]; then
