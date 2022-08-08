a5df963d8f0baedfa6a0d3d87c384573c8c73c2a
Ganapathy Duraiswamy <ganadurai@google.com>
2022-08-08 12:11:15 -0500

    setup-standalone-envoy.sh - lint fixes

--- tools/apigee-envoy-quickstart/scripts/setup-standalone-envoy.sh
+++ tools/apigee-envoy-quickstart/scripts/setup-standalone-envoy.sh
@@ -23,7 +23,7 @@ sleep 5
 if [[ -z $PIPELINE_TEST ]]; then
 
   nohup docker run \
-  -v $CLI_HOME/config.yaml:/config.yaml \
+  -v "$CLI_HOME"/config.yaml:/config.yaml \
   -p 5000:5000 google/apigee-envoy-adapter:v2.0.5 &
 
 else
@@ -50,19 +50,19 @@ docker ps -a
 
 sleep 5
 
-mkdir $ENVOY_CONFIGS_HOME/logs
+mkdir "$ENVOY_CONFIGS_HOME"/logs
 
-chmod 777 $ENVOY_CONFIGS_HOME/logs
+chmod 777 "$ENVOY_CONFIGS_HOME"/logs
 
-cd $ENVOY_CONFIGS_HOME
-sed -i "s/localhost/127.0.0.1/g" $ENVOY_CONFIGS_HOME/envoy-config.yaml
+cd "$ENVOY_CONFIGS_HOME"
+sed -i "s/localhost/127.0.0.1/g" "$ENVOY_CONFIGS_HOME"/envoy-config.yaml
 
-chmod 644 $ENVOY_CONFIGS_HOME/envoy-config.yaml
+chmod 644 "$ENVOY_CONFIGS_HOME"/envoy-config.yaml
 
 if [[ -z $PIPELINE_TEST ]]; then
 
-    nohup docker run --net=host -v $ENVOY_CONFIGS_HOME/logs:/tmp/logs \
-    -v $ENVOY_CONFIGS_HOME/envoy-config.yaml:/etc/envoy/envoy.yaml \
+    nohup docker run --net=host -v "$ENVOY_CONFIGS_HOME"/logs:/tmp/logs \
+    -v "$ENVOY_CONFIGS_HOME"/envoy-config.yaml:/etc/envoy/envoy.yaml \
     -p 8080:8080 \
     --rm envoyproxy/envoy:v1.21-latest --log-path /tmp/logs/custom.log \
     -c /etc/envoy/envoy.yaml --log-level debug &
