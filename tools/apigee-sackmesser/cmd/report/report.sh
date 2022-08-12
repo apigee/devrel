4918807f562424105c7e24398b41399a67b67d5a
Devashish Patil <devashishpatil@google.com>
2022-08-12 15:25:20 +0530

    add kvms in report

--- tools/apigee-sackmesser/cmd/report/report.sh
+++ tools/apigee-sackmesser/cmd/report/report.sh
@@ -408,6 +408,60 @@ done <   <(find "$export_folder/apigeelint/sharedflows/"*.json -print0)
 
 echo "</tbody></table></div></div>" >> "$report_html"
 
+echo "<h2>Configurations</h2>" >> "$report_html"
+
+loginfo "Exporting Configurations"
+
+if [ ! -d "$export_folder/$organization/config/resources/edge/env/$environment" ]; then
+    mkdir -p "$export_folder/$organization/config/resources/edge/env/$environment"
+    echo "<p><i>No Configurations found in organization $organization</i></p>" >> "$report_html"
+fi
+
+echo "<h3>Key Value Maps</h3>" >> "$report_html"
+
+mkdir -p "$export_folder/$organization/config/resources/edge/env/$environment/kvm"
+
+sackmesser list "organizations/$organization/environments/$environment/keyvaluemaps"| jq -r -c '.[]|.' | while read -r kvmname; do
+        sackmesser list "organizations/$organization/environments/$environment/keyvaluemaps/$kvmname" > "$export_folder/$organization/config/resources/edge/env/$environment/kvm/$kvmname".json
+        elem_count=$(jq '.entries? | length' "$export_folder/$organization/config/resources/edge/env/$environment/kvm/$kvmname".json)
+        if [ "$elem_count" -ge "1000" ]; then logwarn "KVMs in Apigee Edge are limited to 1000 entries."; fi
+    done
+
+if ls "$export_folder/$organization/config/resources/edge/env/$environment/kvm"/*.json 1> /dev/null 2>&1; then
+    jq -n '[inputs]' "$export_folder/$organization/config/resources/edge/env/$environment/kvm"/*.json > "$export_folder/$organization/config/resources/edge/env/$environment/kvms".json
+fi
+
+echo "<div><table id=\"kvm-lint\" data-toggle=\"table\" class=\"table\">" >> "$report_html"
+echo "<thead class=\"thead-dark\"><tr>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"id\">Name</th>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"rev\">Encypted</th>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"policies\">Number of Keys</th>" >> "$report_html"
+echo "</tr></thead>" >> "$report_html"
+
+echo "<tbody class=\"mdc-data-table__content\">" >> "$report_html"
+
+jq -c '.[]' "$export_folder/$organization/config/resources/edge/env/$environment/kvms".json | while read i; do 
+    kvmName=$(echo "$i" | jq -r '.name')
+    _encrypted=$(echo "$i" | jq -r '.encrypted')
+    keyCount=1
+
+    if [ $_encrypted = true ]
+        then
+            encrypted="✅"
+        else
+            encrypted="X"
+    fi
+
+    echo "<tr class=\"$highlightclass\">"  >> "$report_html"
+    # echo "<th scope=\"row\"><a href=\"$(resource_link "sharedflows/$sfname" "$linkrevision")\" target=\"_blank\">$sfname<a></th>" >> "$report_html"
+    echo "<td>$kvmName</td>"  >> "$report_html"
+    echo "<td>"$encrypted"</td>"  >> "$report_html"
+    echo "<td>$keyCount</td>" >> "$report_html"
+    echo "</tr>"  >> "$report_html"
+done
+
+echo "</tbody></table></div></div>" >> "$report_html"
+
 cat "$SCRIPT_FOLDER/static/footer.html" >> "$report_html"
 
 loginfo "Sackmesser report is ready in: $report_html"
