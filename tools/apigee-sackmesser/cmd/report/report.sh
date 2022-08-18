ec79e17cc874adf6f57d2eee38c84b6f44f5dde1
Devashish Patil <devashishpatil@google.com>
2022-08-18 12:06:56 +0530

    Add API products to report

--- tools/apigee-sackmesser/cmd/report/report.sh
+++ tools/apigee-sackmesser/cmd/report/report.sh
@@ -549,8 +549,52 @@ jq -c '.[]' "$export_folder/$organization/config/resources/edge/env/$environment
     echo "</tr>"  >> "$report_html"
 done
 
-echo "</tbody></table></div></div>" >> "$report_html"
+echo "</tbody></table></div>" >> "$report_html"
 
+echo "<h2>Organization Configurations</h2>" >> "$report_html"
+loginfo "Exporting Org Configurations"
+
+echo "<h3>API Products</h3>" >> "$report_html"
+
+mkdir -p "$export_folder/$organization/config/resources/edge/env/$environment/api-products"
+
+sackmesser list "organizations/$organization/apiproducts"| jq -r -c '.[]|.' | while read -r apiProductName; do
+        sackmesser list "organizations/$organization/apiproducts/$apiProductName" > "$export_folder/$organization/config/resources/edge/env/$environment/api-products/$apiProductName".json
+        elem_count=$(jq '.entries? | length' "$export_folder/$organization/config/resources/edge/env/$environment/api-products/$apiProductName".json)
+        if [ "$elem_count" -ge "1000" ]; then logwarn "Target Servers in Apigee Edge are limited to 1000 entries."; fi
+    done
+
+if ls "$export_folder/$organization/config/resources/edge/env/$environment/api-products"/*.json 1> /dev/null 2>&1; then
+    jq -n '[inputs]' "$export_folder/$organization/config/resources/edge/env/$environment/api-products"/*.json > "$export_folder/$organization/config/resources/edge/env/$environment/api-products".json
+fi
+
+echo "<div><table id=\"ts-lint\" data-toggle=\"table\" class=\"table\">" >> "$report_html"
+echo "<thead class=\"thead-dark\"><tr>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"id\">Product Name</th>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"envs\">Environments</th>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"proxies\">Proxies</th>" >> "$report_html"
+echo "<th data-sortable=\"true\" data-field=\"enabled\">Aproval Type</th>" >> "$report_html"
+echo "</tr></thead>" >> "$report_html"
+
+echo "<tbody class=\"mdc-data-table__content\">" >> "$report_html"
+
+jq -c '.[]' "$export_folder/$organization/config/resources/edge/env/$environment/api-products".json | while read i; do 
+    apiProductName=$(echo "$i" | jq -r '.name')
+    envs=$(echo "$i" | jq -r '.environments')
+    proxies=$(echo "$i" | jq -r '.proxies')
+    approvalType=$(echo "$i" | jq -r '.approvalType')
+
+    echo "<tr class=\"$highlightclass\">"  >> "$report_html"
+    # echo "<th scope=\"row\"><a href=\"$(resource_link "sharedflows/$sfname" "$linkrevision")\" target=\"_blank\">$sfname<a></th>" >> "$report_html"
+    echo "<td>$apiProductName</td>"  >> "$report_html"
+    echo "<td>"$envs"</td>"  >> "$report_html"
+    echo "<td>$proxies</td>" >> "$report_html"
+    echo "<td>$approvalType</td>" >> "$report_html"
+    echo "</tr>"  >> "$report_html"
+done
+
+echo "</tbody></table></div>" >> "$report_html"
+
+echo "</div>" >> "$report_html"
 cat "$SCRIPT_FOLDER/static/footer.html" >> "$report_html"
-
 loginfo "Sackmesser report is ready in: $report_html"
