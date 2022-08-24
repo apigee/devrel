4a72b74c2fb568e8813c66312d93e619c620a634
Ganapathy Duraiswamy <ganadurai@google.com>
2022-08-24 10:06:08 -0500

    skip sa role additions for pipeline run

--- tools/apigee-envoy-quickstart/scripts/service-accounts.sh
+++ tools/apigee-envoy-quickstart/scripts/service-accounts.sh
@@ -13,19 +13,41 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+addAxRole() {
+  gcloud projects add-iam-policy-binding "$APIGEE_PROJECT_ID" --member \
+    "serviceAccount:$ENVOY_AX_SA@$APIGEE_PROJECT_ID.iam.gserviceaccount.com" \
+    --role "roles/apigee.analyticsAgent"
+}
+
+getAxRoles() {
+  ROLES_ASSIGNED=$(gcloud projects get-iam-policy "$APIGEE_PROJECT_ID" --flatten="bindings[].members" \
+    --format='table(bindings.role)' \
+    --filter="bindings.members:abcd@$APIGEE_PROJECT_ID.iam.gserviceaccount.com" | \
+    grep -c "roles/apigee.analyticsAgent")
+
+  export ROLES_ASSIGNED;
+}
+
 FOUND_SA_IN_PROJECT="$(gcloud iam service-accounts list --filter "$ENVOY_AX_SA" \
   --format="value(email)"  --project "$APIGEE_PROJECT_ID" | grep -w "^$ENVOY_AX_SA")"
 if [ -z "$FOUND_SA_IN_PROJECT" ]; then
     gcloud iam service-accounts create "$ENVOY_AX_SA" \
     --project="$APIGEE_PROJECT_ID"
-fi
-gcloud projects add-iam-policy-binding "$APIGEE_PROJECT_ID" --member \
-"serviceAccount:$ENVOY_AX_SA@$APIGEE_PROJECT_ID.iam.gserviceaccount.com" --role "roles/apigee.analyticsAgent"
 
-gcloud projects get-iam-policy "$APIGEE_PROJECT_ID" --flatten="bindings[].members" \
---format='table(bindings.role)' \
---filter="bindings.members:$ENVOY_AX_SA@$APIGEE_PROJECT_ID.iam.gserviceaccount.com" \
-| grep "roles/apigee.analyticsAgent"
+    addAxRole;
+fi
+
+getAxRoles;
+if [[ $ROLES_ASSIGNED == 0 ]] && [[ -z $PIPELINE_TEST ]]; then
+  addAxRole;
+fi
+
+getAxRoles;
+if [[ $ROLES_ASSIGNED == 0 ]]; then
+  echo "Needed roles not assigned, exiting"
+  exit 1;
+fi
 
 gcloud iam service-accounts keys create "$AX_SERVICE_ACCOUNT" \
 --project="$APIGEE_PROJECT_ID" \
