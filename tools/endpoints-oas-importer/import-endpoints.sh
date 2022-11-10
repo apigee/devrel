3d5ed4211f6ac3049abb8b8dce9a81a153082f5f
Daniel Strebel <strebel@google.com>
2022-11-10 14:56:44 +0100

    chore: directly fail importer on errro

--- tools/endpoints-oas-importer/import-endpoints.sh
+++ tools/endpoints-oas-importer/import-endpoints.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 SCRIPT_FOLDER=$( (cd "$(dirname "$0")" && pwd ))
 
 while [ "$#" -gt 0 ]; do
