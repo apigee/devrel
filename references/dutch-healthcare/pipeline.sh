302a52d05c7a50b2260692b03c77c7e6cf3caab0
Daniel Strebel <strebel@google.com>
2022-12-07 18:12:18 +0100

    chore: deploy dutch-healthcare with fresh token

--- references/dutch-healthcare/pipeline.sh
+++ references/dutch-healthcare/pipeline.sh
@@ -27,11 +27,13 @@ export APIGEE_TOKEN
 ARGS=$*
 SACKMESSER_ARGS="${ARGS:---googleapi}"
 
-sackmesser deploy \
-  -d healthcare-mock-v1 "$SACKMESSER_ARGS"
+APIGEE_TOKEN="$(gcloud config config-helper --force-auth-refresh --format json | jq -r '.credential.access_token')"
 
 sackmesser deploy \
-  -d healthcare-v1 "$SACKMESSER_ARGS"
+  -d healthcare-mock-v1 "$SACKMESSER_ARGS" -t "$APIGEE_TOKEN"
+
+sackmesser deploy \
+  -d healthcare-v1 "$SACKMESSER_ARGS" -t "$APIGEE_TOKEN"
 
 npm i --no-fund --prefix healthcare-v1
 TEST_BASEPATH='/healthcare/v1' TEST_HOST="$APIGEE_X_HOSTNAME" npm test --prefix healthcare-v1
