7ccef07a097041fc323388433dce7f8abca32a8a
anaik91 <ashwinknaik@google.com>
2023-08-14 20:08:43 +0530

    feat: addressed flake8 for xorhybrid proxy-endpoint-unifier wrapper

--- tools/proxy-endpoint-unifier/xorhybrid.py
+++ tools/proxy-endpoint-unifier/xorhybrid.py
@@ -17,24 +17,26 @@
 import requests
 import os
 
+
 class ApigeeXorHybrid:
-    def __init__(self,org):
-        self.baseurl=f"https://apigee.googleapis.com/v1/organizations/{org}"
+    def __init__(self, org):
+        self.baseurl = f"https://apigee.googleapis.com/v1/organizations/{org}"
         self.auth_header = {}
 
-    def set_auth_header(self,token):
+    def set_auth_header(self, token):
         self.auth_header = {
-            'Authorization' : f"Bearer {token}"
+            'Authorization': f"Bearer {token}"
         }
-    
-    def validate_api(self,api_type,proxy_bundle_path):
+
+    def validate_api(self, api_type, proxy_bundle_path):
         api_name = os.path.basename(proxy_bundle_path).split('.zip')[0]
-        url = f"{self.baseurl}/{api_type}?name={api_name}&action=validate&validate=true"
-        files=[
-        ('data',(api_name,open(proxy_bundle_path,'rb'),'application/zip'))
+        url = f"{self.baseurl}/{api_type}?name={api_name}&action=validate&validate=true"  # noqa
+        files = [
+            ('data', (api_name, open(proxy_bundle_path,'rb'), 'application/zip'))  # noqa
         ]
-        response = requests.request("POST", url, headers=self.auth_header, data={}, files=files)
-        if response.status_code == 200 :
+        response = requests.request("POST", url, headers=self.auth_header,
+                                    data={}, files=files)
+        if response.status_code == 200:
             return True
         else:
             return response.json()
